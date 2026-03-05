4e484c4e3aec07a612fa77de446afdb7520d4a80
reytdn <reynaldotuden88@gmail.com>
2026-03-05 08:38:38 +0800

    database app

---
+++ index.php
@@ -0,0 +1,22 @@
+<!-- 
+views contain all interfaces
+controller waiter bakod ay 
+model kitchen everything is in there all your ingredients
+all querying in the model
+
+all interface
+
+every form in modules will have its corresponding js file
+job of js to read content of form
+
+
+htaccess- hide folder subfolder 
+
+-> = . sa java
+mag  access method
+-->
+
+<?php
+require_once "controllers/template.controller.php";
+$template = new ControllerTemplate();
+$template -> ctrTemplate();
